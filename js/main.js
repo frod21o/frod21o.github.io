d428c0712aeeaa80f55bb47d02f046f78b102fb0
Jedrzej <tcankon@gmail.com>
2025-05-14 18:25:04 +0200

    remove 2 second wait

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 window.addEventListener('load', () => {
     'use strict';
   
-    // Fade-in po 2 sek.
-    setTimeout(() => {
-      document.body.style.opacity = '1';
-    }, 2000);
-  
     // Rejestracja service workera
         console.log('out if');
     if ('serviceWorker' in navigator) {
